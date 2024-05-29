b0a5942edb307f99d4a7798ed63cfb31beb6390d
Alastair Clements <77981196+alastair-clements@users.noreply.github.com>
2024-05-28 21:44:39 -0400

    Update text_from_image.py
    Weird workaround to force streamlit to install tesseract

--- streamlit_apps/text_from_image.py
+++ streamlit_apps/text_from_image.py
@@ -2,18 +2,36 @@ import streamlit as st
 from PIL import Image
 import pytesseract
 import pandas as pd
+import subprocess
 import shutil
 
+def install_tesseract():
+    try:
+        subprocess.run(['apt-get', 'update'], check=True)
+        subprocess.run(['apt-get', 'install', '-y', 'tesseract-ocr'], check=True)
+    except subprocess.CalledProcessError as e:
+        st.error("An error occurred while installing Tesseract: {e}")
+        return False
+    return True
 
-# Dynamically find Tesseract executable
+# Check if Tesseract is installed and install if not found
 tesseract_cmd = shutil.which("tesseract")
 if tesseract_cmd:
     pytesseract.pytesseract.tesseract_cmd = tesseract_cmd
 else:
-    st.error("Tesseract not found. Ensure it is installed and in your PATH.")
+    st.info("Tesseract not found. Installing...")
+    if install_tesseract():
+        tesseract_cmd = shutil.which("tesseract")
+        if tesseract_cmd:
+            pytesseract.pytesseract.tesseract_cmd = tesseract_cmd
+        else:
+            st.error("Tesseract installation failed. Ensure it is installed and in your PATH.")
+    else:
+        st.error("Tesseract installation failed. Ensure it is installed and in your PATH.")
 
 def extract_text_from_image(image):
     try:
+        # Use pytesseract to do OCR on the image
         text = pytesseract.image_to_string(image)
         return text
     except Exception as e:
@@ -34,15 +52,17 @@ def process_images_to_csv(images, output_csv):
             st.error(f"Error processing {filename}: {e}")
             continue
 
-    # Extracts data to a DF and writes to csv
+    # Create a DataFrame from the extracted data
     df = pd.DataFrame(list(zip(f, t)), columns=['file_Name', 'Text'])
+
+    # Write the DataFrame to a CSV file
     df.to_csv(output_csv, index=False)
     return df
 
 def main():
     st.title("Image Text Extractor")
 
-    # File uploader
+    # File uploader for multiple image files
     uploaded_files = st.file_uploader("Choose image files", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
 
     if uploaded_files:
