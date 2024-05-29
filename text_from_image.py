66935781b8e687072c4a22d4dc60b1dcc5d9c238
Alastair Clements <77981196+alastair-clements@users.noreply.github.com>
2024-05-28 21:08:21 -0400

    Update text_from_image.py
    Got rid of some dumb comments

--- text_from_image.py
+++ text_from_image.py
@@ -4,8 +4,7 @@ import pytesseract
 import pandas as pd
 import os
 
-# Ensure tesseract is installed and available in PATH, or provide the correct path
-# Use the path returned by `which tesseract`
+# Defines tesseract path
 pytesseract.pytesseract.tesseract_cmd = "/opt/homebrew/bin/tesseract"
 
 def extract_text_from_image(image):
@@ -31,17 +30,15 @@ def process_images_to_csv(images, output_csv):
             st.error(f"Error processing {filename}: {e}")
             continue
 
-    # Create a DataFrame from the extracted data
+    # Adds extracted data to a dataframe and adds it to a csv
     df = pd.DataFrame(list(zip(f, t)), columns=['file_Name', 'Text'])
-
-    # Write the DataFrame to a CSV file
     df.to_csv(output_csv, index=False)
     return df
 
 def main():
     st.title("Image Text Extractor")
 
-    # File uploader for multiple image files
+    # Creates uploader for image files.
     uploaded_files = st.file_uploader("Choose image files", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
 
     if uploaded_files:
